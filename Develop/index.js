a49d0f2c35249862339c75ff540a394ceecf8bd5
Matt <39172048+magiccreb@users.noreply.github.com>
2022-12-15 13:36:17 -0500

    added some basic questions to the questions array and plugged a few into inquirer to test. tinkered with inquirer output.

--- Develop/index.js
+++ Develop/index.js
@@ -3,13 +3,29 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the name of your application?', 'Please provide a brief description of your application.', 'Please provide installation instructions for your application.', 'Please provide a brief description of how your application is used.'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'appName',
+            message: questions[0],
+        },
+        {
+            type: 'input',
+            name: 'appDesc',
+            message: questions[1],
+        },
+    ])
+    .then((responses) => {
+    console.log(responses);
+    });
+}
 
 // Function call to initialize app
 init();
